8b813d623079fea3a50ef96f6b763709dfd37635
Animesh245 <bdanimesh245@gmail.com>
2022-07-26 22:39:40 +0600

    Entity Class

---
+++ Backend/src/main/java/com/animesh245/backend/model/Employee.java
@@ -0,0 +1,31 @@
+package com.animesh245.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbl_employee")
+public class Employee
+{
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String email;
+
+
+}
