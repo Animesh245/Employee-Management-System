9dbf4cd3da29a6e0135d641af9caf6eb29b34b27
Animesh245 <bdanimesh245@gmail.com>
2022-07-28 18:27:36 +0600

    HttpClientModule

--- Frontend/src/main/frontend/src/app/app.module.ts
+++ Frontend/src/main/frontend/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http' // for using httpclient to connect to restapi
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
