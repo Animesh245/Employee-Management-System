ef6dcc09f293eff869e258858da801fe3fdbf170
Animesh245 <bdanimesh245@gmail.com>
2022-07-29 13:14:39 +0600

    @CrossOrigin

--- Backend/src/main/java/com/animesh245/backend/controller/EmployeeController.java
+++ Backend/src/main/java/com/animesh245/backend/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")// when modules are run separately
 public class EmployeeController {
 
     @Autowired
