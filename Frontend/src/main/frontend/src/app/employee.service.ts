7748edd998660f6ffc1f0f22df561ac9864fb898
Animesh245 <bdanimesh245@gmail.com>
2022-07-29 15:16:49 +0600

    Service Class

---
+++ Frontend/src/main/frontend/src/app/employee.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  
+  private baseURL = "http://localhost:8080/api/v1/employees";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getEmployeesList(): Observable<Employee[]>{
+    return this.httpClient.get<Employee[]>(`${this.baseURL}`);
+  }
+
+  //this is a post method, will be used to send data to api
+  createEmployee(employee: Employee): Observable<Object>{
+    return this.httpClient.post<Object>(`${this.baseURL}`, employee);
+  }
+
+  getEmployeeById(id: number): Observable<Employee>{
+    return this.httpClient.get<Employee>(`${this.baseURL}/${id}`);
+  }
+
+  updateEmployee(id: number, employee: Employee): Observable<Object>{
+    return this.httpClient.put<Object>(`${this.baseURL}/${id}`, employee);
+  }
+
+  deleteEmployee(id: number): Observable<Object>{
+    return this.httpClient.delete(`${this.baseURL}/${id}`);
+  }
+}
