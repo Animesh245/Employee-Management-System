977340e05061c20fc0cf33267d87d81e3e47c1cf
Animesh245 <bdanimesh245@gmail.com>
2022-07-29 17:11:17 +0600

    App Routing module file

--- Frontend/src/main/frontend/src/app/app-routing.module.ts
+++ Frontend/src/main/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './employee-details/employee-details.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    //by default routing
+    {path:'', redirectTo: 'employees', pathMatch: 'full'},
+  //for all employees
+  {path: 'employees', component: EmployeeListComponent},
+  //routing for create-employee
+  {path: 'create-employee', component: CreateEmployeeComponent},
+  //for update-employee
+  {path: 'update-employee/:id', component: UpdateEmployeeComponent},
+  //for view-employee details
+  {path: 'employee-details/:id', component: EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
