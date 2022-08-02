8a41a55d3acce1e3dd714975a9c51bdb881c2a0b
Animesh245 <bdanimesh245@gmail.com>
2022-08-02 22:00:00 +0600

    File Upload using nio package

--- Backend/src/main/java/com/animesh245/backend/service/FileService.java
+++ Backend/src/main/java/com/animesh245/backend/service/FileService.java
@@ -8,6 +8,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 
 @Service
@@ -24,19 +27,21 @@ public class FileService{
 
         try {
 
-            //IF using Stream Api
+//            //IF using only Stream Api
+//            //read
+//            InputStream is = file.getInputStream();
+//            byte [] data = new byte[is.available()];
+//            is.read(data);
+//
+//            //write
+//            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator + file.getOriginalFilename());
+//            fos.write(data);
+//
+//            fos.flush();
+//            fos.close();
 
-            //read
-            InputStream is = file.getInputStream();
-            byte [] data = new byte[is.available()];
-            is.read(data);
-
-            //write
-            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator + file.getOriginalFilename());
-            fos.write(data);
-
-            fos.flush();
-            fos.close();
+            //If using NIO package and InputStream
+            Files.copy(file.getInputStream(), Paths.get(UPLOAD_DIR+File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
 
             status=true;
         }catch (Exception e)
