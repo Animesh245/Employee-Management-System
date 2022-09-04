7ebda9dacd48534744e00304cb6158ead65e6e21
Animesh245 <bdanimesh245@gmail.com>
2022-09-05 04:12:50 +0600

    Refactor

--- Backend/src/main/java/com/animesh245/backend/exception/NotFoundException.java
+++ Backend/src/main/java/com/animesh245/backend/exception/NotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class EmployeeNotFoundException extends RuntimeException
+public class NotFoundException extends RuntimeException
 {
-    public EmployeeNotFoundException(Long id)
+    public NotFoundException(String  id)
     {
         super("Could not find Employee " + id);
     }
