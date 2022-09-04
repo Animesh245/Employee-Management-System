339a6d6f8610116694a17a1aa8d84f95f92c0dba
Animesh245 <bdanimesh245@gmail.com>
2022-09-05 04:11:59 +0600

    file storage location

---
+++ Backend/src/main/java/com/animesh245/backend/config/Properties.java
@@ -0,0 +1,7 @@
+package com.animesh245.backend.config;
+
+public class Properties
+{
+    public static final int MAX_UPLOAD_SIZE = (1 * 1024 * 1024) * 10; // 1MB * 10 = 10 MB
+    public static final String WRITE_PATH = System.getProperty("user.home") + "/Documents/EMS_Files/";
+}
