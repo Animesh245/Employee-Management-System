9640d44b6c9389c72910b368d46b39c8b5301145
Animesh245 <bdanimesh245@gmail.com>
2022-09-09 00:33:49 +0600

    Replaced ServletUriComponentsBuilder.fromCurrentContextPath() with file.getOriginalFilename() Only file name will be stored in the db, before uploading file it will check the file and folder repo exits or not

--- Backend/src/main/java/com/animesh245/backend/service/implementation/FileServiceImpl.java
+++ Backend/src/main/java/com/animesh245/backend/service/implementation/FileServiceImpl.java
@@ -4,11 +4,12 @@ import com.animesh245.backend.config.Properties;
 import com.animesh245.backend.service.definition.FileService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
@@ -19,29 +20,24 @@ public class FileServiceImpl implements FileService {
 
     public String uploadFile(MultipartFile file)
     {
+        Path rootLocation = Paths.get(Properties.WRITE_PATH);
+        if (!file.isEmpty()) {
+            String fileName = file.getOriginalFilename();
+            assert fileName != null;
+            Path destinationFile = rootLocation.resolve(fileName);
 
-        try {
 
-//            //IF using only Stream Api
-//            //read
-//            InputStream is = file.getInputStream();
-//            byte [] data = new byte[is.available()];
-//            is.read(data);
-//
-//            //write
-//            FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+ File.separator + file.getOriginalFilename());
-//            fos.write(data);
-//
-//            fos.flush();
-//            fos.close();
+            if (!destinationFile.toFile().exists()) {
+                destinationFile.toFile().mkdirs();
+            }
 
-            //If using NIO package and InputStream
-            Files.copy(file.getInputStream(), Paths.get(Properties.WRITE_PATH +File.separator+file.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
-
-        }catch (Exception e)
-        {
-            e.printStackTrace();
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return ServletUriComponentsBuilder.fromCurrentContextPath().path(Properties.WRITE_PATH +File.separator+file.getOriginalFilename()).toUriString();
+
+        return file.getOriginalFilename();
     }
 }
