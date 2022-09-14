28441bf362b19de0533a3986e86b88c59663d320
Animesh245 <bdanimesh245@gmail.com>
2022-09-14 14:39:59 +0600

    Allowed all urls as origin. Any url can do operation with the api.

--- Backend/src/main/java/com/animesh245/backend/controller/definition/AppController.java
+++ Backend/src/main/java/com/animesh245/backend/controller/definition/AppController.java
@@ -1,11 +1,11 @@
 package com.animesh245.backend.controller.definition;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping(value = "/api/v1")
-@CrossOrigin(origins = "http://localhost:4200")// when modules are run separately
+@RequestMapping(value = "/public")
+@CrossOrigin// when modules are run separately
 public interface AppController
 {
-    @GetMapping("/hello")
+    @GetMapping(value = "/")
     String hello();
 
 }
